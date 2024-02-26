91948cffe61f0a4952d843cce095d508bdc2c507
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-02-26 10:53:47 +0800

    feat: add user playlist subscription modules

---
+++ Modules/Playlist/Database/Seeders/PlaylistDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Playlist\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PlaylistDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Playlist/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('playlist::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('playlist.name') !!}</p>
+@endsection
