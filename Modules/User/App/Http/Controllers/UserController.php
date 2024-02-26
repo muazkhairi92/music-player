e3dee3dedbe32702fdd05a80a924497c45871736
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-02-26 11:14:26 +0800

    feat: auth and user controllers logic

--- Modules/User/App/Http/Controllers/UserController.php
+++ Modules/User/App/Http/Controllers/UserController.php
@@ -3,9 +3,12 @@
 namespace Modules\User\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Modules\User\App\Http\Requests\RegisterRequest;
+use Modules\User\App\Http\Requests\UpdateUserRequest;
+use Modules\User\App\Http\Resources\UserResource;
+use Modules\User\App\Models\User;
 
 class UserController extends Controller
 {
@@ -14,54 +17,52 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('user::index');
+        $user = Auth::user();
+        $perPage = request('per_page') ?? 15;
+        $search = request('search');
+
+        $users = User::where('id', '!=', $user->id)
+            ->when(request('search'), function ($q) use ($search) {
+                $q->where(function ($qu) use ($search) {
+                    $qu->where('name', 'like', "%$search%")
+                    ->orWhere('email', 'like', "%$search%");
+                });
+            })
+            ->paginate($perPage);
+
+        return $this->sendResponse('users list', UserResource::collection($users)->response()->getData(), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(RegisterRequest $request)
     {
-        return view('user::create');
+        $user = User::create($request->all());
+        $user->refresh();
+        $user->subscription()->create();
+
+        return $this->sendResponse('User creation successful.', new UserResource($user), 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): RedirectResponse
+    public function update(UpdateUserRequest $request)
     {
-        //
-    }
+        $user = Auth::user();
+        if (Hash::check($request->old_password, $user->password)) {
+            $user->update($request->all());
 
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
-    {
-        return view('user::show');
-    }
+            return $this->sendResponse('User updated successfully', new UserResource($user), 200);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('user::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id): RedirectResponse
-    {
-        //
+        return $this->sendError('Failed too update user', null, 400);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy($id)
+    public function destroy()
     {
-        //
+        $user = Auth::user();
+
+        $user->delete();
+
+        return $this->sendResponse('successfully deleted user', null, 200);
     }
 }
