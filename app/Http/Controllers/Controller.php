be366d35a45ef0637cb058c1eee87a18553d767a
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-02-26 11:20:18 +0800

    fix: controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,10 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
+
+    /**
+     * Create new sucess JSON response instance.
+     *
+     * @param string     $message
+     * @param array|null $data
+     * @param int        $code
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($message, $data, $code = 200)
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ];
+
+        return response()->json($response, $code);
+    }
+
+    /**
+     * Create new error JSON response instance.
+     *
+     * @param string     $message
+     * @param array|null $data
+     * @param int        $code
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendError($message, $data = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+        ];
+
+        if (!empty($data)) {
+            $response['data'] = $data;
+        }
+
+        return response()->json($response, $code);
+    }
 }
