178e75973e9e8206a2a9fc637601da3263f8caca
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-02-26 15:15:11 +0800

    fix: endpoint playlist

--- Modules/Playlist/routes/api.php
+++ Modules/Playlist/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Playlist\App\Http\Controllers\PlaylistController;
 
 /*
     |--------------------------------------------------------------------------
